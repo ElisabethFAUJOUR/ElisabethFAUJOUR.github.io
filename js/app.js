864711ecc176abd922585cf9703a8d8f9fcb03ad
Elisabeth FAUJOUR <efaujour@gmail.com>
2023-07-13 11:18:43 +0200

    fade in

--- js/app.js
+++ js/app.js
@@ -12,12 +12,37 @@ const app = {
      * Smooth scrool between sections
      */
     smoothScroll() {
+        app.observeElements();
         app.listenToClickNavLinks();
         app.listenToClickLogoLink();
         app.listenToClickScrollArrow();
     },
 
     // ----- LISTENER EVENTS -----
+    /**
+     * Smooth elements display when scrolling page 
+     */
+    observeElements() {
+        const elements = document.querySelectorAll(".fade-in");
+        const options = {
+          root: null, // élement racine utilisé comme zone d'intersection, "null" => fenêtre du navigateur 
+          rootMargin: "0px", // marge supplémentaire 
+          threshold: 0.2 // l'élément doit être au moins 20% visible pour être considérer comme intersectant 
+        };
+      
+        const observer = new IntersectionObserver((entries, observer) => { // instance de IntersectionObserver avec pour paramètre un callback
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add("fade-in-visible");
+              observer.unobserve(entry.target);
+            }
+          });
+        }, options);
+      
+        elements.forEach(element => {
+          observer.observe(element);
+        });
+      },
 
     /**
      * Listener click on nav bar links and smooth scroll
